aa30606ce3b5c7d184dbbb9c6fd34e7628826e83
ade <ade@lemon.(none)>
2013-02-05 06:10:44 +0700

    jalanin ajah

---
+++ potensi/report/reportNU/src/runReport.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ade
+ */
+public class runReport {
+
+    static boolean debugging = true;
+
+    public static void main(String args[]) {
+        String direktoryReport = "/media/apps/pakedy/depak/potensi/report/";
+        if (!debugging) {
+            direktoryReport = "";
+        }
+        String fileReport ="",qryData ="",thisCondition="";
+        java.util.Map parameter = new java.util.HashMap();
+        
+        fileReport = direktoryReport + "newaasetLaporan.jasper";
+        qryData = "select id,jenis_aset,ket_jenis,nama_aset,lokasi,ranting,kecid"
+                + ",(select b.namaKelurahan from tbkelurahan b where b.kelurahanID=kelid) as namaKabupaten"
+                + ",(select a.namaKecamatan from tbkecamatan a where a.kecamatanID=kecid) as namaKecamatan"
+                + "  from newaset"+thisCondition+" order by kecid,id";
+        
+        String klaSS = "jdbc:mysql://localhost/potensi?user=nujatim&password=klaser";
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            java.sql.Connection conec = java.sql.DriverManager.getConnection(klaSS);
+            parameter.put("qryData", qryData);
+            net.sf.jasperreports.engine.JasperPrint jasperPrint = net.sf.jasperreports.engine.JasperFillManager.fillReport(fileReport, parameter, conec);
+            net.sf.jasperreports.view.JasperViewer jasperViewer = new net.sf.jasperreports.view.JasperViewer(jasperPrint, false);
+            jasperViewer.setDefaultCloseOperation(javax.swing.JFrame.HIDE_ON_CLOSE);
+            jasperViewer.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+            jasperViewer.setTitle("Laporan Aset");
+            if (debugging) {
+                jasperViewer.setVisible(true);
+            }
+        } catch (Exception ex) {
+            if (debugging) {
+                System.out.println(" error " + ex.getMessage());
+            }
+        }
+    }
+}
